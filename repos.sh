d1b78b07f7228c8ed19a8b95e7ecfc13ed5432e5
Jaime Melis <jmelis@redhat.com>
2019-09-26 09:13:07 +0200

    refactor the curl command

--- repos.sh
+++ repos.sh
@@ -4,11 +4,10 @@
 [ -z "$APP_INTERFACE_PASSWORD" ] && echo "Please define APP_INTERFACE_PASSWORD env var" && exit 1
 
 # APP_INTERFACE_URL default value
-APP_INTERFACE_URL=${APP_INTERFACE_URL:-app-interface.devshift.net/graphql}
+[ -z "${APP_INTERFACE_URL}" ] && APP_INTERFACE_URL="https://app-interface.devshift.net/graphql"
 
 QUERY='{"query":"{ apps_v1 { codeComponents { url, resource }}}"}'
-API_URL="https://${APP_INTERFACE_USER}:${APP_INTERFACE_PASSWORD}@${APP_INTERFACE_URL}"
 
-curl -s -H 'Content-Type: application/json' --data-binary "$QUERY" ${API_URL} | \
+curl -s -H 'Content-Type: application/json' --user "${APP_INTERFACE_USER}:${APP_INTERFACE_PASSWORD}" --data-binary "$QUERY" "${APP_INTERFACE_URL}" | \
     jq -r '.data.apps_v1[]|select(.codeComponents)|.codeComponents[]|select((.resource == "upstream") or (.resource == "saasrepo"))|.url' | \
     sort -u
