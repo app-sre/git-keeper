6818d8cbd2b22e1f1ea3b5d44bb057c3bf8d2785
Serhii Kryzhnii <skryzhni@redhat.com>
2019-09-30 18:04:38 +0300

    added upload to S3 and GPG encryption

--- git-keeper.py
+++ git-keeper.py
@@ -11,6 +11,10 @@ import sh
 import boto3
 import botocore
 import shutil
+import logging
+from datetime import datetime
+import sys
+import gnupg
 
 # bake some commands
 mirror = sh.git.clone.bake('--mirror')
@@ -18,11 +22,17 @@ tar = sh.tar.bake('-cf')
 
 workdir = 'workdir'
 
-def upload2s3(repo_tar):
-#    toBdefined
-    print()
+def upload2s3(repo_tar, DATE, object_name):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(repo_tar, GIT_KEEPER_BUCKET, DATE + '/' + object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 def cleanwrkdir(workdir):
+    os.makedirs(workdir, exist_ok=True)
     shutil.rmtree(workdir)
     os.makedirs(workdir, exist_ok=True)
 
@@ -31,35 +41,41 @@ def clone_repo(repo_url, repo_dir):
 #    print(REPO_URL)
     mirror(REPO_URL, repo_dir)
 
-def __init__(self):
-    self.boto3_aws = boto3.session.Session()
+
+def main():
+
+    DATE = datetime.now().strftime('%Y-%m-%d')
     try:
-        self.boto3_aws.client('sts').get_caller_identity()
+        s3_client = boto3.client('s3')
     except botocore.exceptions.NoCredentialsError:
         raise Exception('No AWS credentials found.')
     except botocore.exceptions.ClientError:
         raise Exception('Invalid AWS credentials.')
+    s3 = boto3.resource('s3')
+    if s3.Bucket(GIT_KEEPER_BUCKET).creation_date is None:
+        raise Exception('Bucket \'{}\' doesn\'t exist'.format(GIT_KEEPER_BUCKET))
+    gpg = gnupg.GPG()
 
-def main():
-
-   repolist = open("repolist", 'r')
-
-   for repo_raw in repolist:
-        repo = repo_raw.rstrip()
+    repolist = sys.stdin.read().splitlines()
+    for repo in repolist:
         repo_url = repo + '.git'
         repo_dir = workdir + '/' + os.path.basename(repo_url)
         repo_tar = repo_dir + '.tar'
         cleanwrkdir(workdir)
         clone_repo(repo_url, repo_dir)
         tar(repo_tar, repo_dir)
-        upload2s3(repo_tar)
+        repo_gpg = repo_tar + '.gpg'
+        with open(repo_tar, 'rb') as f:
+            status = gpg.encrypt_file(
+                f, recipients=['skryzhni@redhat.com', 'jmelis@redhat.com'],
+                output=repo_gpg,
+                armor=False)
+        upload2s3(repo_gpg, DATE, os.path.basename(repo_url) + '.tar.gpg')
 #        cleanwrkdir(workdir)
-   repolist.close()
 
 if __name__ == '__main__':
-     try:
-        GIT_KEEPER_BUCKET = os.environ['GIT_KEEPER_BUCKET'] 
-     except Exception:
+    try:
+        GIT_KEEPER_BUCKET = os.environ['GIT_KEEPER_BUCKET']
+    except Exception:
         raise Exception('Invalid GIT_KEEPER_BUCKET Env.')
-     main()
-
+    main()
