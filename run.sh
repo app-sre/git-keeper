e35b9a80e0ee22f105b45f3ed1e17b8f119f2db0
Jaime Melis <jmelis@redhat.com>
2019-10-09 12:33:27 +0200

    pass gpg keys

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#
+
 # Required secrets (vault env vars)
 # - $CONFIG_TOML
+# - $GPG_KEYS
 
 CONFIG_DIR="$PWD/config"
-mkdir -p $CONFIG_DIR/gpg
+mkdir -p $CONFIG_DIR/
 
 # get repos
 bash repos.sh > repos.txt
 
-# get gpgs -- fetch_gpg.sh script will query app-interface
-# download the target gpg keys and put them
-# in the $CONFIG_DIR/gpg folder (first argument of the script)
-bash fetch_gpg.sh $CONFIG_DIR/gpg 
+# dump gpg keys to file
+echo "$GPG_KEYS" | base64 -d > $CONFIG_DIR/gpg_keys
 
 # get config.toml -- includes s3/gitlab creds
 echo "$CONFIG_TOML" | base64 -d > $CONFIG_DIR/config.toml
@@ -21,4 +20,4 @@ cat repos.txt | docker run --rm -i \
             -v $CONFIG_DIR:/config:z \
             quay.io/app-sre/git-keeper:latest \
             --config /config/config.toml \
-            --gpgs /config/gpg
+            --gpg-keys /config/gpg_keys
