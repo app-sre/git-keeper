9a4d65a341d5da1c7e194621a60f9fd820a8bc85
Jaime Melis <jmelis@redhat.com>
2019-10-10 13:23:53 +0200

    fix env vars

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@
 # Required secrets (vault env vars)
 # - $CONFIG_TOML
 # - $GPG_KEYS
+# - $CONFIG_NETRC
+# - $USERNAME_PRODUCTION
+# - $PASSWORD_PRODUCTION
 
 CONFIG_DIR="$PWD/config"
 mkdir -p $CONFIG_DIR/
 
 # get repos
+export APP_INTERFACE_USER="${USERNAME_PRODUCTION}"
+export APP_INTERFACE_PASSWORD="${PASSWORD_PRODUCTION}"
 bash repos.sh > repos.txt
 
 # dump gpg keys to file
@@ -17,7 +22,7 @@ echo "$GPG_KEYS" | base64 -d > $CONFIG_DIR/gpg_keys
 echo "$CONFIG_TOML" | base64 -d > $CONFIG_DIR/config.toml
 
 # hack for .netrc
-echo "CONFIG_NETRC" | base64 -d > $CONFIG_DIR/.netrc
+echo "$CONFIG_NETRC" | base64 -d > $CONFIG_DIR/.netrc
 chmod 0666 $CONFIG_DIR/.netrc
 
 cat repos.txt | docker run --rm -i \
