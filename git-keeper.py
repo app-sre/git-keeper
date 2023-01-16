e971ee4ca7ab587256e9514ebc41ff3e0a2deb0f
Ryan Wallace <rywallac@redhat.com>
2023-01-16 13:52:49 -0500

    black reformat

--- git-keeper.py
+++ git-keeper.py
@@ -24,9 +24,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # bake some commands
-clone_repo = sh.git.clone.bake('--mirror')
-tar = sh.tar.bake('-cf')
-workdir = 'workdir'
+clone_repo = sh.git.clone.bake("--mirror")
+tar = sh.tar.bake("-cf")
+workdir = "workdir"
 
 
 def cleanwrkdir(workdir):
@@ -36,59 +36,62 @@ def cleanwrkdir(workdir):
 
 def get_s3_client(aws_access_key_id, aws_secret_access_key):
     try:
-        s3_client = boto3.client('s3',
-                                 aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key)
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        )
     except botocore.exceptions.NoCredentialsError:
-        raise Exception('No AWS credentials found.')
+        raise Exception("No AWS credentials found.")
     except botocore.exceptions.ClientError:
-        raise Exception('Invalid AWS credentials.')
+        raise Exception("Invalid AWS credentials.")
     return s3_client
 
 
-def git_clone_upload(s3_client, gpg, recipients,
-                     repo_url, s3_bucket, subfolders, date):
-    logger.info('Processing repo: %s', repo_url)
-    if not repo_url.endswith('.git'):
-        repo_url = repo_url + '.git'
-        logger.debug('Appending .git to repo')
+def git_clone_upload(s3_client, gpg, recipients, repo_url, s3_bucket, subfolders, date):
+    logger.info("Processing repo: %s", repo_url)
+    if not repo_url.endswith(".git"):
+        repo_url = repo_url + ".git"
+        logger.debug("Appending .git to repo")
     repo_dir = os.path.join(workdir, os.path.basename(repo_url))
-    repo_tar = repo_dir + '.tar'
-    logger.debug('Clearing workdir')
+    repo_tar = repo_dir + ".tar"
+    logger.debug("Clearing workdir")
     cleanwrkdir(workdir)
-    logger.debug('Clonning repo')
+    logger.debug("Clonning repo")
     clone_repo(repo_url, repo_dir)
-    logger.debug('TARing repo')
+    logger.debug("TARing repo")
     tar(repo_tar, repo_dir)
-    repo_gpg = repo_tar + '.gpg'
-    with open(repo_tar, 'rb') as f:
-        logger.debug('Encrypting repo\'s tar')
+    repo_gpg = repo_tar + ".gpg"
+    with open(repo_tar, "rb") as f:
+        logger.debug("Encrypting repo's tar")
         gpg.encrypt_file(
-            f, recipients=recipients,
-            output=repo_gpg,
-            armor=False,
-            always_trust=True)
-    object_name = urlparse(repo_url).netloc + \
-        urlparse(repo_url).path + '.tar.gpg'
+            f, recipients=recipients, output=repo_gpg, armor=False, always_trust=True
+        )
+    object_name = urlparse(repo_url).netloc + urlparse(repo_url).path + ".tar.gpg"
     for subfolder in subfolders:
-        logger.info('Uploading repo: %s to subfolder: %s', repo_gpg, subfolder)
-        s3_client.upload_file(repo_gpg, s3_bucket, os.path.join(
-            subfolder, date, object_name))
+        logger.info("Uploading repo: %s to subfolder: %s", repo_gpg, subfolder)
+        s3_client.upload_file(
+            repo_gpg, s3_bucket, os.path.join(subfolder, date, object_name)
+        )
     cleanwrkdir(workdir)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Configuration TOML and GPG keys locations.')
-    parser.add_argument('--config', type=str, required=True,
-                        help='Path of configuration TOML file')
-    parser.add_argument('--gpgs', type=str, required=True,
-                        help='Path of GPG keys file')
-    parser.add_argument('--subfolders', type=str, default='',
-                        help='Path of [comma delimited] subfolder[s]'
-                        ' in bucket to store backups')
+        description="Configuration TOML and GPG keys locations."
+    )
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path of configuration TOML file"
+    )
+    parser.add_argument("--gpgs", type=str, required=True, help="Path of GPG keys file")
+    parser.add_argument(
+        "--subfolders",
+        type=str,
+        default="",
+        help="Path of [comma delimited] subfolder[s]" " in bucket to store backups",
+    )
     args = parser.parse_args()
-    subfolders = [str(subfolder) for subfolder in args.subfolders.split(',')]
+    subfolders = [str(subfolder) for subfolder in args.subfolders.split(",")]
 
     cnf = toml.load(open(args.config))
     aws_access_key_id = cnf["s3"]["aws_access_key_id"]
@@ -96,21 +99,22 @@ def main():
     s3_bucket = cnf["s3"]["bucket"]
     s3_client = get_s3_client(aws_access_key_id, aws_secret_access_key)
 
-    date = datetime.now().strftime('%Y-%m-%d--%H-%M')
+    date = datetime.now().strftime("%Y-%m-%d--%H-%M")
 
     gpg = gnupg.GPG()
     with open(args.gpgs) as f:
         key_data = f.read()
     gpg.import_keys(key_data)
-    recipients = [k['fingerprint'] for k in gpg.list_keys()]
+    recipients = [k["fingerprint"] for k in gpg.list_keys()]
 
-    logger.info('Process started')
+    logger.info("Process started")
     repolist = sys.stdin.read().splitlines()
     error = False
     for repo in repolist:
         try:
-            git_clone_upload(s3_client, gpg, recipients, repo,
-                             s3_bucket, subfolders, date)
+            git_clone_upload(
+                s3_client, gpg, recipients, repo, s3_bucket, subfolders, date
+            )
         except Exception as e:
             git_hub_private_repo_error_text = (
                 "fatal: could not read Username for 'https://github.com': "
@@ -120,11 +124,11 @@ def main():
                 error = True
                 logging.error(e)
             else:
-                logger.warning('Skipping private github repo: %s', repo)
+                logger.warning("Skipping private github repo: %s", repo)
 
     if error:
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- repos.py
+++ repos.py
@@ -11,29 +11,29 @@ from requests.auth import HTTPBasicAuth
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # GraphQL
-GRAPHQL_SERVER_BASE_URL = os.environ['GRAPHQL_SERVER_BASE_URL']
-GRAPHQL_USERNAME = os.environ['GRAPHQL_USERNAME']
-GRAPHQL_PASSWORD = os.environ['GRAPHQL_PASSWORD']
+GRAPHQL_SERVER_BASE_URL = os.environ["GRAPHQL_SERVER_BASE_URL"]
+GRAPHQL_USERNAME = os.environ["GRAPHQL_USERNAME"]
+GRAPHQL_PASSWORD = os.environ["GRAPHQL_PASSWORD"]
 
 CODE_COMPONENTS_QUERY = "{ apps: apps_v1 { codeComponents { url, resource }}}"
 GITLAB_BACKUP_ORGS_QUERY = "{ gitlabs: gitlabinstance_v1 { backupOrgs }}"
 
 # GitLab
-GITLAB_SERVER = os.environ['GITLAB_SERVER']
-GITLAB_TOKEN = os.environ['GITLAB_TOKEN']
+GITLAB_SERVER = os.environ["GITLAB_SERVER"]
+GITLAB_TOKEN = os.environ["GITLAB_TOKEN"]
 
 
 def gql_query(query):
     auth = HTTPBasicAuth(GRAPHQL_USERNAME, GRAPHQL_PASSWORD)
     url = "https://{}/graphql".format(GRAPHQL_SERVER_BASE_URL)
-    response = requests.get(url, params={'query': query}, auth=auth)
+    response = requests.get(url, params={"query": query}, auth=auth)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def git_suffix(repo):
-    if not repo.endswith('.git'):
-        repo += '.git'
+    if not repo.endswith(".git"):
+        repo += ".git"
     return repo
 
 
@@ -41,10 +41,10 @@ def get_codecomponents():
     data = gql_query(CODE_COMPONENTS_QUERY)
 
     return [
-        git_suffix(cc['url'])
-        for app in data['apps']
-        for cc in (app['codeComponents'] or [])
-        if cc['resource'] in ('upstream', 'saasrepo')
+        git_suffix(cc["url"])
+        for app in data["apps"]
+        for cc in (app["codeComponents"] or [])
+        if cc["resource"] in ("upstream", "saasrepo")
     ]
 
 
@@ -53,26 +53,25 @@ def get_gitlab_backup_orgs():
 
     # TODO: this is a stop-gap measure. The proper fix is to return the url in
     # the app-interface query and the token (vault secret).
-    if len(data['gitlabs']) != 1:
-        raise Exception('Expecting only one gitlab.')
+    if len(data["gitlabs"]) != 1:
+        raise Exception("Expecting only one gitlab.")
 
-    return data['gitlabs'][0].get('backupOrgs', [])
+    return data["gitlabs"][0].get("backupOrgs", [])
 
 
 def get_gitlab_org_repos(org):
-    headers = {'Private-Token': GITLAB_TOKEN}
-    base_url = os.path.join(GITLAB_SERVER, 'api/v4')
+    headers = {"Private-Token": GITLAB_TOKEN}
+    base_url = os.path.join(GITLAB_SERVER, "api/v4")
 
-    url = os.path.join(base_url, 'groups', org, 'projects')
+    url = os.path.join(base_url, "groups", org, "projects")
 
     items = []
 
     per_page = 100
     page = 1
     while True:
-        params = {'page': page, 'per_page': per_page}
-        response = requests.get(
-            url, params=params, headers=headers, verify=False)
+        params = {"page": page, "per_page": per_page}
+        response = requests.get(url, params=params, headers=headers, verify=False)
         response.raise_for_status()
         response_items = response.json()
 
@@ -83,7 +82,7 @@ def get_gitlab_org_repos(org):
 
         page += 1
 
-    return [git_suffix(repo['http_url_to_repo']) for repo in items]
+    return [git_suffix(repo["http_url_to_repo"]) for repo in items]
 
 
 def main():
@@ -99,5 +98,5 @@ def main():
         print(repo)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
